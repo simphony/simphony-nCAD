e12c10790746f53226282ac8c65f6046db33724d
SGGgarcia <ggarcia@sgenia.com>
2015-07-01 10:05:55 +0200

    Fixed doc.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -258,3 +258,18 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# Read-the-docs Mock, so we can build the documentation:
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['simncad']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
+
+
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-WWelcome to SimPhoNy nCad's documentation!
+Welcome to SimPhoNy nCad's documentation!
 =========================================
 
 .. include:: ../../README.md
